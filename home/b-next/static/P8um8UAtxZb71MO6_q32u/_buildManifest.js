4736eab479dcd4b009808f45d151ddba2b35a387
sunil_malakar <sunil.malakar@angara.com>
2024-01-26 01:07:08 +0530

    new package with update build

--- home/b-next/static/P8um8UAtxZb71MO6_q32u/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST=function(e,s){return{__rewrites:{afterFiles:[{has:e,source:s,destination:s},{has:e,source:"/blog",destination:e},{has:e,source:"/blog/:path*",destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/_app","/_error"]}}(void 0,"/:path*"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
