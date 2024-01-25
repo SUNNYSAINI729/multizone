b65630e7e416d1add5f4af399f7eadcc2ec89702
sunil_malakar <sunil.malakar@angara.com>
2024-01-26 00:58:00 +0530

    new build created

---
+++ home/b-next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"yAKFqw5uTDJQdIf/xe3CpgUBOqsZGSUktGChzaFPAEw=\"}"
--- home/bnext/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"RsxHOWcYzC17pRtA6z3razsaC2Tc99Y7uf0e74S39jM=\"}"
